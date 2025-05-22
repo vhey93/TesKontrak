5f29fbd6abdc68beebb8fc068550e8da1c7f2964
verizone <35033009+vhey93@users.noreply.github.com>
2025-05-22 21:11:12 +0700

    Update and rename MOONCASH.sol to Swap.sol

---
+++ Swap.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract FakeSwap {
+    event Swapped(address indexed user, uint256 amountIn, uint256 amountOut);
+
+    function swap(uint256 amountIn) public {
+        require(amountIn > 0, "Amount must be > 0");
+        emit Swapped(msg.sender, amountIn, amountIn * 2);
+    }
+}
