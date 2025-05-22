ff841cf2aa5d3f8f57980728e90e3f807e3051c9
verizone <35033009+vhey93@users.noreply.github.com>
2025-05-22 21:12:34 +0700

    Create Vote.sol

---
+++ Vote.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract FakeVote {
+    mapping(address => uint256) public votes;
+    event Voted(address indexed voter, uint256 option);
+
+    function vote(uint256 option) public {
+        require(option <= 3, "Invalid option");
+        votes[msg.sender] = option;
+        emit Voted(msg.sender, option);
+    }
+}
